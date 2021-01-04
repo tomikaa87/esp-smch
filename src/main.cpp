20b66bc60799d4831b2625b467d4ec402f0d5d54
tomikaa87 <tomikaa87@gmail.com>
2021-01-04 11:52:10 +0100

    Support Arduino OTA password

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ void setup()
 {
     static ApplicationConfig appConfig;
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 0, 1 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 0, 2 };
 
     appConfig.blynk.appToken = Config::Blynk::AppToken;
     appConfig.blynk.serverHostName = Config::Blynk::ServerHostName;
@@ -24,6 +24,7 @@ void setup()
 
     appConfig.otaUpdate.updateCheckIntervalMs = 60000;
     appConfig.otaUpdate.updateUrl = Config::OtaUpdate::FirmwareUpdateUrl;
+    appConfig.otaUpdate.arduinoOtaPasswordHash = Config::OtaUpdate::ArduinoOtaPasswordHash;
 
     appConfig.wifi.password = Config::WiFi::Password;
     appConfig.wifi.ssid = Config::WiFi::SSID;
